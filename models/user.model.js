f94498b72f4eb961c18002f14f01b28230a3d977
Arvind Sangwan <asangwan8998@gmail.com>
2023-02-28 21:33:42 -0500

    Added db files

--- models/user.model.js
+++ models/user.model.js
@@ -1,42 +1,46 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: false,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: false,
+    },
+    desc: {
+      type: String,
+      required: false,
+    },
+    isSeller: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  img: {
-    type: String,
-    required: false,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: false,
-  },
-  desc: {
-    type: String,
-    required: false,
-  },
-  isSeller: {
-    type: Boolean,
-    default: false,
-  },
-  timestamps,
-});
+  {
+    timestamps: true,
+  }
+);
 
-export default model("User", userSchema);
+export default model("user", userSchema);
--- server.js
+++ server.js
@@ -2,6 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import gigRoutes from "./routes/gig.route.js";
+import orderRoutes from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
+
 const app = express();
 dotenv.config();
 mongoose.set("strictQuery", true);
@@ -13,12 +20,14 @@ try {
   console.log(error);
 }
 
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/orders", userRoutes);
+app.use("/api/gigs", gigRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
 
 app.listen(8000, () => {
   console.log("Server is running");
